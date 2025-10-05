10b3ac5ecbdf2c562923ef7f0ceaf5a42befdac9
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-10-05 22:58:28 +0100

    Simplify statement

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ for i in range(9):
 
     print("It is", s+"'s turn")
 
-    while (p := input("Enter position (1-9) ")) and 0 or not p.isdigit() or (p := int((int(p)-1)*1.35))*0 or p < 0 or p > 10 or (b[p] != "-" and (print("That space is taken") or 1)):
+    while (p := input("Enter position (1-9) ")) and not p.isdigit() or (p := int(p)+int(p)//3-1)*0 or p < 0 or p > 10 or b[p] != "-" and (print("That space is taken") or 1):
         1
 
     b[p] = s
