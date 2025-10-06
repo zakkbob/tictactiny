3647c9189b94c6bb3e4ff5fe7f681e94913ddb70
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-10-06 18:43:16 +0100

    Change storage method things

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-b,i,s,p=list("---\n"*3),0,'',0
-while not(print(''.join(b))or any([set([b[j]for j in L])=={s}for L in[range(p//4,p//4+3),[p%4,p%4+4,p%4+8],[0,5,10],[2,5,8]]])and(print(s,"has won")or 1)or(i>8 and(print("Draw!")or 1))or print("It is",(s:="OX"[(i:=i+1)%2])+"'s turn")or((any((not((p:=input("Enter position (1-9) "))and 0 or not p.isdigit()or(p:=int(p)+(int(p)-1)//3-1)*0 or p<0 or p>10 or b[p]!="-"and(print("That space is taken")or 1))for _ in range(9**9))))*0)):
-    b[p]=s
+b,i,s,p=[[],[]],0,'',0
+while not(print(''.join(["-OX"[(k in b[0])+2*(k in b[1])] if k%4!=3 else '\n' for k in range(12)]))or any([all([j in b[i%2] for j in L])for L in[range(p//4,p//4+3),[p%4,p%4+4,p%4+8],[0,5,10],[2,5,8]]])and(print(s,"has won")or 1)or(i>8 and(print("Draw!")or 1))or print("It is",(s:="OX"[(i:=i+1)%2])+"'s turn")or
+       ((any((not((p:=input("Enter position (1-9) "))and 0 or not p.isdigit()or(p:=int(p)+(int(p)-1)//3-1)*0 or p<0 or p>10 or (p in b[1] or p in b[0])and(print("That space is taken")or 1))for _ in range(9**9))))*0)):
+    b[i%2].append(p)
