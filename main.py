e7acc1bd7ad8282d29f27808870f322053205c44
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-10-06 07:57:56 +0100

    Remove a line; 3 lines now. I doubt it can get much less

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 b,i,s,p=list("---\n"*3),0,'',0
-while not(any([set([b[j]for j in L])=={s}for L in[range(p//4,p//4+3),[p%4,p%4+4,p%4+8],[0,5,10],[2,5,8]]])and(print(s,"has won")or 1)or(i>8 and (print("Draw!")or 1))or print("It is",(s:="OX"[(i:=i+1)%2])+"'s turn")or ((any((not((p:=input("Enter position (1-9) "))and 0 or not p.isdigit()or(p:=int(p)+(int(p)-1)//3-1)*0 or p<0 or p>10 or b[p]!="-"and(print("That space is taken")or 1))for _ in range(9**9))))*0)):
+while not(print(''.join(b))or any([set([b[j]for j in L])=={s}for L in[range(p//4,p//4+3),[p%4,p%4+4,p%4+8],[0,5,10],[2,5,8]]])and(print(s,"has won")or 1)or(i>8 and(print("Draw!")or 1))or print("It is",(s:="OX"[(i:=i+1)%2])+"'s turn")or((any((not((p:=input("Enter position (1-9) "))and 0 or not p.isdigit()or(p:=int(p)+(int(p)-1)//3-1)*0 or p<0 or p>10 or b[p]!="-"and(print("That space is taken")or 1))for _ in range(9**9))))*0)):
     b[p]=s
-    print(''.join(b))
